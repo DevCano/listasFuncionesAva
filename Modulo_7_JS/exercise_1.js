c66228cde31c50b120bfd56374b487a37b096db7
Nazario Cano <1026187a@umich.mx>
2023-03-30 21:29:02 -0600

    Ejercicio 1 de modulo 7 de conjuntos

---
+++ Modulo_7_JS/exercise_1.js
@@ -0,0 +1,11 @@
+// - Un nuevo Set con los nombres de tu familia
+const names = new Set(['Marivel', 'Jose Angel', 'Abel', 'Efrain']);
+console.log(names);
+
+// - Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+names.add('Efrain');
+console.log(names);
+
+// - Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
+names.add('Javascript');
+console.log(names);
